e9db2a7f7b4f4ccef4176777c5d2282a58c64852
Sandy Maguire <sandy@sandymaguire.me>
2018-12-21 00:10:09 -0500

    the inspiration is definitely flowing now!!!!!

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,11 @@
 
 module Lib where
 
+import Control.Monad
 import Control.Applicative
 import Euterpea.Music
 import Euterpea.IO.MIDI.Play
+import System.IO.Unsafe
 
 deriving instance Foldable Music
 deriving instance Foldable Primitive
@@ -19,6 +21,7 @@ maj7 = curry $ \r ->
   , trans 11 r
   ]
 
+
 min7 :: PitchClass -> Octave -> [Pitch]
 min7 = curry $ \r ->
   [ r
@@ -26,6 +29,7 @@ min7 = curry $ \r ->
   , trans 7 r
   , trans 10 r
   ]
+
 
 dom7 :: PitchClass -> Octave -> [Pitch]
 dom7 = curry $ \r ->
@@ -39,31 +43,29 @@ dom7 = curry $ \r ->
 musicChord :: Dur -> [Pitch] -> Music Pitch
 musicChord d = chord . fmap (note d)
 
-progression :: [[Pitch]]
-progression =
-  [ min7 D 4
-  , maj7 C 4
-  , maj7 Bf 4
-  , dom7 A 4
+
+addSemitonesToPitchClass :: PitchClass -> Int -> PitchClass
+addSemitonesToPitchClass pc st = fst $ trans st (pc, 4)
+
+
+progression :: PitchClass -> [[Pitch]]
+progression key =
+  [ min7 key  4
+  , dom7 (addSemitonesToPitchClass key 2) 4
+  , maj7 (addSemitonesToPitchClass key 5) 4
+  , maj7 (addSemitonesToPitchClass key 10) 4
   ]
+
+
+leadingProgression :: [[Pitch]] -> [([Pitch], Pitch)]
+leadingProgression p = zipWith (\a b -> (a, head b)) p $ tail $ cycle p
+
 
 data Hand
     = LeftHand Int
     | RightHand Int
+    | GraspingHand Int
   deriving (Eq, Ord, Show)
-
-
-myParadiddle17 :: Music Hand
-myParadiddle17 = line
-  [ note en $ LeftHand 0
-  , note en $ RightHand 1
-  , note en $ RightHand 2
-  , note en $ LeftHand 3
-  , note en $ RightHand 4
-  , note en $ LeftHand 5
-  , note en $ RightHand 6
-  , note en $ RightHand 7
-  ]
 
 
 bind :: (Dur -> a -> Music b) -> Music a -> Music b
@@ -74,11 +76,39 @@ bind f (ma :=: mb)         = bind f ma :=: bind f mb
 bind f (Modify control ma) = Modify control $ bind f ma
 
 
+duration :: Music a -> Dur
+duration (Prim (Note dur _)) = dur
+duration (Prim (Rest dur))   = dur
+duration (a :+: b)           = duration a + duration b
+duration (a :=: b)           = max (duration a) (duration b)
+duration (Modify _ a)        = duration a
+
+
 myBar :: [Pitch] -> Music Hand -> Music Pitch
-myBar ((basspitch, bassoctave):remainder) paradiddle = bind f paradiddle
+myBar chord@((basspitch, bassoctave):remainder) paradiddle = bind f paradiddle
   where
-    f dur (LeftHand _)  = note dur (basspitch, bassoctave - 1)
-    f dur (RightHand _) = musicChord dur remainder
+    f dur (LeftHand i)  = note dur (basspitch, bassoctave - (i `mod` 2 + 1))
+    f dur (RightHand i) = (!! i) $ iterate invert $  musicChord dur remainder
+    f dur (GraspingHand i)  = note dur (fst $ chord !! i, bassoctave + 2)
+
+
+bassJob :: Music Hand
+bassJob = line
+  [ note qn $ LeftHand 0
+  , note qn $ LeftHand 1
+  , chord
+    [ line
+        [ note qn $ LeftHand 2
+        , note qn $ LeftHand 3
+        ]
+    , line
+        [ rest en
+        , note en $ GraspingHand 0
+        , note en $ GraspingHand 1
+        , note en $ GraspingHand 2
+        ]
+    ]
+  ]
 
 
 maybeAToPrim :: Dur -> Maybe a -> Music a
@@ -88,11 +118,10 @@ maybeAToPrim dur (Just a) = note dur a
 
 makeADiddleDoo :: Dur -> [Music Hand]
 makeADiddleDoo totalDur
-  | totalDur < 1/16 = pure $ rest 0
+  | totalDur < 1/16 = pure $ rest totalDur
   | otherwise = do
       dur  <- [ totalDur, totalDur / 2 ]
-      hand <- [ Just $ LeftHand 0
-              , Just $ RightHand 0
+      hand <- [ Just $ RightHand 0
               , Nothing
               ]
       following <- makeADiddleDoo $ totalDur - dur
@@ -103,8 +132,19 @@ makeADiddleDoo totalDur
           ]
 
 
--- (Prim $ Note duration RightHand) ---->
---       ((Prim $ Note duration firstThingIntheTriad) :=: ())
+type Key = (PitchClass, Mode)
 
--- playTheLeftHandBaby :: [Pitch] -> Pitch
+
+-- makeALeadingNote :: Key -> Pitch -> [Pitch]
+-- makeALeadingNote k ch = do
+
+
+theFunk :: Music Pitch
+theFunk = line
+        . zipWith myBar (cycle $ progression A)
+        . fmap (:=: bassJob)
+        -- . drop 14
+        $ makeADiddleDoo $ 1/2
+
+main = playDev 2 theFunk
 
